65de4cd7e73df666b2f4d31c7c564561e92d6688
Termasarus <114614110+Termasarus@users.noreply.github.com>
2022-10-21 06:38:34 -0400

    Create my-script.js

---
+++ my-script.js
@@ -0,0 +1,68 @@
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.plasma = "Plasma";
+  CONFIG.DND5E.damageResistanceTypes.plasma = "Plasma";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.dark = "Dark";
+  CONFIG.DND5E.damageResistanceTypes.dark = "Dark";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.normal = "Normal";
+  CONFIG.DND5E.damageResistanceTypes.normal = "Normal";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.water = "Water";
+  CONFIG.DND5E.damageResistanceTypes.water = "Water";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.grass = "Grass";
+  CONFIG.DND5E.damageResistanceTypes.grass = "Grass";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.electric = "Electric";
+  CONFIG.DND5E.damageResistanceTypes.electric = "Electric";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.ice = "Ice";
+  CONFIG.DND5E.damageResistanceTypes.ice = "Ice";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.fighting = "Fighting";
+  CONFIG.DND5E.damageResistanceTypes.fighting = "Fighting";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.water = "Water";
+  CONFIG.DND5E.damageResistanceTypes.water = "Water";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.ground = "Ground";
+  CONFIG.DND5E.damageResistanceTypes.ground = "Ground";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.flying = "Flying";
+  CONFIG.DND5E.damageResistanceTypes.flying = "Flying";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.bug = "Bug";
+  CONFIG.DND5E.damageResistanceTypes.bug = "Bug";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.rock = "Rock";
+  CONFIG.DND5E.damageResistanceTypes.rock = "Rock";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.ghost = "Ghost";
+  CONFIG.DND5E.damageResistanceTypes.ghost = "Ghost";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.dragon = "Dragon";
+  CONFIG.DND5E.damageResistanceTypes.dragon = "Dragon";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.steel = "Steel";
+  CONFIG.DND5E.damageResistanceTypes.steel = "Steel";
+});
+Hooks.on("setup", () => {
+  CONFIG.DND5E.damageTypes.fairy = "Fairy";
+  CONFIG.DND5E.damageResistanceTypes.fairy = "Fairy";
+});
